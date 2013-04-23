70f171a20e915e12c10f7091807d97ff18cd468b
Christian Nieves <me@christiannieves.com>
2013-04-22 23:15:26 -0600

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Gif_to_Sheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.pbSprite = new System.Windows.Forms.PictureBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.inputFolderButton = new System.Windows.Forms.Button();
+            this.inputFolderBox = new System.Windows.Forms.TextBox();
+            this.exportFolderButton = new System.Windows.Forms.Button();
+            this.outputFolderBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderGoButton = new System.Windows.Forms.Button();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSprite)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(6, 19);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.ReadOnly = true;
+            this.tbFile.Size = new System.Drawing.Size(189, 20);
+            this.tbFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(201, 17);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMain.AutoScroll = true;
+            this.panelMain.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMain.Controls.Add(this.pbSprite);
+            this.panelMain.Location = new System.Drawing.Point(12, 204);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(390, 326);
+            this.panelMain.TabIndex = 5;
+            // 
+            // pbSprite
+            // 
+            this.pbSprite.Location = new System.Drawing.Point(0, 0);
+            this.pbSprite.Name = "pbSprite";
+            this.pbSprite.Size = new System.Drawing.Size(100, 50);
+            this.pbSprite.TabIndex = 0;
+            this.pbSprite.TabStop = false;
+            this.pbSprite.Visible = false;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(201, 40);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 8;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbFile);
+            this.groupBox1.Controls.Add(this.btnExport);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(390, 76);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Single File";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.folderGoButton);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.outputFolderBox);
+            this.groupBox2.Controls.Add(this.exportFolderButton);
+            this.groupBox2.Controls.Add(this.inputFolderBox);
+            this.groupBox2.Controls.Add(this.inputFolderButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 94);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(390, 104);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Folder";
+            // 
+            // inputFolderButton
+            // 
+            this.inputFolderButton.Location = new System.Drawing.Point(292, 18);
+            this.inputFolderButton.Name = "inputFolderButton";
+            this.inputFolderButton.Size = new System.Drawing.Size(83, 23);
+            this.inputFolderButton.TabIndex = 0;
+            this.inputFolderButton.Text = "Choose Folder";
+            this.inputFolderButton.UseVisualStyleBackColor = true;
+            this.inputFolderButton.Click += new System.EventHandler(this.inputFolderButton_Click);
+            // 
+            // inputFolderBox
+            // 
+            this.inputFolderBox.Location = new System.Drawing.Point(51, 20);
+            this.inputFolderBox.Name = "inputFolderBox";
+            this.inputFolderBox.ReadOnly = true;
+            this.inputFolderBox.Size = new System.Drawing.Size(235, 20);
+            this.inputFolderBox.TabIndex = 2;
+            // 
+            // exportFolderButton
+            // 
+            this.exportFolderButton.Location = new System.Drawing.Point(292, 47);
+            this.exportFolderButton.Name = "exportFolderButton";
+            this.exportFolderButton.Size = new System.Drawing.Size(83, 23);
+            this.exportFolderButton.TabIndex = 3;
+            this.exportFolderButton.Text = "Choose Folder";
+            this.exportFolderButton.UseVisualStyleBackColor = true;
+            this.exportFolderButton.Click += new System.EventHandler(this.exportFolderButton_Click);
+            // 
+            // outputFolderBox
+            // 
+            this.outputFolderBox.Location = new System.Drawing.Point(51, 49);
+            this.outputFolderBox.Name = "outputFolderBox";
+            this.outputFolderBox.ReadOnly = true;
+            this.outputFolderBox.Size = new System.Drawing.Size(235, 20);
+            this.outputFolderBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input";
+            // 
+            // folderGoButton
+            // 
+            this.folderGoButton.Location = new System.Drawing.Point(336, 75);
+            this.folderGoButton.Name = "folderGoButton";
+            this.folderGoButton.Size = new System.Drawing.Size(39, 23);
+            this.folderGoButton.TabIndex = 7;
+            this.folderGoButton.Text = "Go!";
+            this.folderGoButton.UseVisualStyleBackColor = true;
+            this.folderGoButton.Click += new System.EventHandler(this.folderGoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 542);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panelMain);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GIF to Spritesheet";
+            this.panelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSprite)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.PictureBox pbSprite;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox inputFolderBox;
+        private System.Windows.Forms.Button inputFolderButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox outputFolderBox;
+        private System.Windows.Forms.Button exportFolderButton;
+        private System.Windows.Forms.Button folderGoButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Gif_to_Sheet
+{
+    public partial class Form1 : Form
+    {
+
+        Image gif;
+        Bitmap bitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "GIF Images (*.gif)|*.gif";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                gif = Image.FromFile(ofd.FileName);
+                tbFile.Text = ofd.FileName;
+                pbSprite.Visible = true;
+                FormatImage();
+            }
+        }
+
+        private void FormatImage()
+        {
+            if (gif == null)
+                return;
+            if (bitmap != null)
+                bitmap.Dispose();
+            FrameDimension frameSize = new FrameDimension(gif.FrameDimensionsList[0]);
+            Size imageSize = new Size(gif.Size.Width, gif.Size.Height);
+            int frames = gif.GetFrameCount(frameSize);
+            int calculatedColumns = (int)Math.Round(Math.Sqrt(frames));
+            int columns = (int)calculatedColumns < 1 ? frames : (int)calculatedColumns;
+            int rows = (int)Math.Ceiling((double)frames / columns);
+            bitmap = new Bitmap(columns * imageSize.Width, rows * imageSize.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            SolidBrush brush = new SolidBrush(Color.Transparent);
+            g.FillRectangle(brush, new Rectangle(0, 0, bitmap.Width, bitmap.Height));
+            for (int i = 0; i < frames; i++)
+            {
+                gif.SelectActiveFrame(frameSize, i);
+                g.DrawImage(gif, i % columns * imageSize.Width, i / columns * imageSize.Height);
+            }
+            g.Dispose();
+            pbSprite.Image = bitmap;
+            pbSprite.Size = bitmap.Size;
+            this.Refresh();
+        }
+
+        private void nudColumns_ValueChanged(object sender, EventArgs e)
+        {
+            FormatImage();
+        }
+
+        private void pbBackColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Color = Color.Transparent;
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                FormatImage();
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|JPEG Files (*.jpeg)|*.jpeg|GIF Files (*.gif)|*.gif";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                ImageFormat format;
+                string ext = Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                    case ".jpeg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                    case ".gif":
+                        format = ImageFormat.Gif;
+                        break;
+                    default:
+                        format = ImageFormat.Png;
+                        break;
+                }
+                pbSprite.Image.Save(sfd.FileName, format);
+            }
+        }
+
+        private void inputFolderButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputFolderBox.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void exportFolderButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    outputFolderBox.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void folderGoButton_Click(object sender, EventArgs e)
+        {
+            string[] fileEntries = Directory.GetFiles(inputFolderBox.Text);
+            foreach (string filename in fileEntries)
+            {
+
+                gif = Image.FromFile(filename);
+                pbSprite.Visible = true;
+                FormatImage();
+                pbSprite.Image.Save(Path.Combine(outputFolderBox.Text,Path.GetFileNameWithoutExtension(filename) + ".png"), ImageFormat.Png);
+                Console.WriteLine("Wrote : " + Path.GetFileNameWithoutExtension(filename) + "*.png");
+            }
+            MessageBox.Show("Operation complete");
+        }
+    }
+}
